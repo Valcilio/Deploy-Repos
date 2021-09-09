ea57af9ed77095dd13fccce03f606c628c26923e
Valcílio Júnior <82968972+Valcilio@users.noreply.github.com>
2021-09-09 06:25:49 -0300

    Add files via upload

---
+++ ApiHandler.py
@@ -0,0 +1,36 @@
+import pickle
+import pandas as pd
+from sklearn.preprocessing  import RobustScaler, MinMaxScaler
+from flask import Flask, request, Response
+from insuranceAll.InsuranceAll import InsuranceAll
+
+model = pickle.load(open('models/lgbm_final.pkl','rb'))
+
+app = Flask(__name__)
+@app.route('/', methods=['POST'])
+
+def insurance_all_predict():
+    test_json= request.get_json()
+    
+    if test_json:
+        if isinstance(test_json,dict):
+            test_raw=pd.DataFrame(test_json,index=[0])
+        else:
+            test_raw=pd.DataFrame(test_json,columns=test_json[0].keys())
+               
+        pipeline = InsuranceAll()
+        df = test_raw.copy()
+        df = pipeline.data_cleaning(df)
+        df = pipeline.feature_engieneering(df)
+        df = pipeline.feature_filtering(df)
+        df = pipeline.data_rescaling(df)
+        df = pipeline.data_encoding(df)
+        df_response = pipeline.get_prediction(model, test_raw, df)
+        return df_response
+    
+    else:
+        return Response('{}', status=200, mimetype='application/json')
+
+    
+if __name__ == '__main__':
+    app.run()
